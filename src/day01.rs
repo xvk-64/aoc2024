ab1cf675745089a366b2d637e3dbc993c1e6c2a3
xvk-64 <18544814+xvk-64@users.noreply.github.com>
2024-12-02 21:48:12 +1030

    Day 1 Part 2

--- src/day01.rs
+++ src/day01.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::fs;
 use std::path::Path;
 
@@ -23,4 +24,15 @@ fn main() {
     let diff = (0..list1.len()).map(|i| (list1[i] - list2[i]).abs()).sum::<i32>();
 
     println!("Part 1: Difference: {}", diff);
+
+    let mut counts: HashMap<i32, i32> = HashMap::new();
+    for elem in list2 {
+        counts.entry(elem).and_modify(|e| *e += 1).or_insert(1);
+    }
+
+    let similarity = list1.iter().map(|elem| {
+        elem * (counts.get(&elem).unwrap_or(&0))
+    }).sum::<i32>();
+    
+    println!("Part 2: Similarity: {}", similarity);
 }
