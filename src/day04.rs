6350db50fc958c5728eaa650815371146f842782
xvk-64 <18544814+xvk-64@users.noreply.github.com>
2024-12-04 18:06:17 +1030

    Day 4 part 2

--- src/day04.rs
+++ src/day04.rs
@@ -1,24 +1,66 @@
 use std::fs;
 use std::path::Path;
 
-fn traverse_xmas(matrix: &Vec<Vec<char>>, pos: &(usize, usize), dir: (isize, isize)) -> bool {
+fn add_within_matrix(matrix: &Vec<Vec<char>>, mut pos: (usize, usize), dir: &(isize, isize)) -> Option<(usize, usize)> {
+    let new_y = pos.1 as isize + dir.1;
+    let new_x = pos.0 as isize + dir.0;
+
+    if new_x < 0 || new_y < 0 {return None;}
+
+    pos.1 = new_y as usize;
+    pos.0 = new_x as usize;
+
+    if pos.1 >= matrix.len() || pos.0 >= matrix[pos.1].len() {return None;}
+
+    Some(pos)
+}
+
+fn traverse_str(matrix: &Vec<Vec<char>>, s: &str, pos: &(usize, usize), dir: (isize, isize)) -> bool {
     let mut pos = pos.clone();
 
-    for c in "XMAS".chars() {
+    for c in s.chars() {
         if matrix[pos.1][pos.0] != c {return false;}
-        
-        if c == 'S' {return true;}
 
-        let new_y = pos.1 as isize + dir.1;
-        let new_x = pos.0 as isize + dir.0;
+        if c == s.chars().last().unwrap() {return true;}
 
-        if new_x < 0 || new_y < 0 {return false;}
-
-        pos.1 = new_y as usize;
-        pos.0 = new_x as usize;
-
-        if pos.1 >= matrix.len() || pos.0 >= matrix[pos.1].len() {return false;}
+        if let Some(new_pos) = add_within_matrix(matrix, pos, &dir) {
+            pos = new_pos;
+        } else {return false;}
     }
+
+    true
+}
+
+fn add_dirs(v1: (isize, isize), v2: (isize, isize)) -> (isize, isize) {
+    ((v1.0 + v2.0).clamp(-1, 1), (v1.1 + v2.1).clamp(-1, 1))
+}
+fn negate_dir(v: &(isize, isize)) -> (isize, isize) {
+    (-v.0, -v.1)
+}
+
+fn get_orthogonal_dir(dir: &(isize, isize)) -> (isize, isize) {
+    (dir.1, -dir.0)
+}
+
+fn find_mas(matrix: &Vec<Vec<char>>, pos: &(usize, usize), dir: &(isize, isize)) -> bool {
+    if matrix[pos.1][pos.0] != 'A' {return false;}
+
+    if let Some(prev_pos) = add_within_matrix(matrix, pos.clone(), &negate_dir(&dir)) {
+        if matrix[prev_pos.1][prev_pos.0] != 'M' {return false;}
+    } else {return false;}
+    if let Some(prev_pos) = add_within_matrix(matrix, pos.clone(), &dir) {
+        if matrix[prev_pos.1][prev_pos.0] != 'S' {return false;}
+    } else {return false;}
+
+    true
+}
+
+fn find_x_mas(matrix: &Vec<Vec<char>>, pos: &(usize, usize), up_dir: (isize, isize)) -> bool {
+    if !(find_mas(matrix, &pos, &up_dir) || find_mas(matrix, &pos, &negate_dir(&up_dir))) {return false;}
+
+    let orth_dir = get_orthogonal_dir(&up_dir);
+
+    if !(find_mas(matrix, &pos, &orth_dir) || find_mas(matrix, &pos, &negate_dir(&orth_dir))) {return false;}
 
     true
 }
@@ -38,26 +80,18 @@ fn main() {
 
     for row in 0..matrix.len() {
         for col in 0..matrix[row].len() {
-            if matrix[row][col] == 'X' {
+            if matrix[row][col] == 'A' {
                 let mut num: u8 = 0;
-    
-                if traverse_xmas(&matrix, &(col, row), (0, 1)) {num+=1;}
-                if traverse_xmas(&matrix, &(col, row), (1, 1)) {num+=1;}
-                if traverse_xmas(&matrix, &(col, row), (1, 0)) {num+=1;}
-                if traverse_xmas(&matrix, &(col, row), (1, -1)) {num+=1;}
-                if traverse_xmas(&matrix, &(col, row), (0, -1)) {num+=1;}
-                if traverse_xmas(&matrix, &(col, row), (-1, -1)) {num+=1;}
-                if traverse_xmas(&matrix, &(col, row), (-1, 0)) {num+=1;}
-                if traverse_xmas(&matrix, &(col, row), (-1, 1)) {num+=1;}
-    
+                // if find_x_mas(&matrix, &(col, row), (0, 1)) {num+=1;}
+                if find_x_mas(&matrix, &(col, row), (1, 1)) {num+=1;}
+
                 matrix[row][col] = num.to_string().chars().nth(0).unwrap();
-    
-                
+
                 num_xmas += num as usize;
             }
         }
         println!("{}", matrix[row].iter().collect::<String>());
     }
 
-    println!("Num XMAS: {}", num_xmas);
+    println!("Num X-MAS: {}", num_xmas);
 }
